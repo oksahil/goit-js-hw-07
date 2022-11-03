bb956dac0c93fef94f880b2a24d3713a7ab56881
oksahil <oksana.khylyk@gmail.com>
2022-11-03 21:45:08 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const rest = {
+div: document.querySelector('div'),
+};
+gallery.addEventListener('click', clickImg);
+
+const clickImg = (e) => {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    
+};
