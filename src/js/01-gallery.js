79598090eca6ca77c375b3807dccb22a8a479166
oksahil <oksana.khylyk@gmail.com>
2022-11-17 21:08:49 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,16 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
-const rest = {
-div: document.querySelector('div'),
-};
-gallery.addEventListener('click', clickImg);
+const galleryContainer = document.querySelector('.gallery');
+const itemMarkup = createGalleryCart(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', createGalleryCart(galleryItems));
 
-const clickImg = (e) => {
+
+function createGalleryCart(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+           return  `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+            </a>
+            </div>
+            `
+        }
+        
+        ).join('');
+}
+
+galleryContainer.addEventListener('click', clickImg);
+
+function clickImg(e) {
+    e.preventDefault();
     if (e.target.nodeName !== 'IMG') {
         return;
-    };
+    }
 
-    
-};
+    const originalImg = basicLightbox.create(`
+    <div class="modal">
+    <a><img src="${e.target.dataset.source}" alt="${e.target.description}"></a>
+    </div>
+`, {
+    onShow: (originalImg) => {
+        originalImg.element().querySelector('a').onclick = originalImg.close
+    }
+})
+
+originalImg.show()
+
+}
+
