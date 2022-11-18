780d6b76b4863ce67f6bcabd1b616a1cc17a2c1a
oksahil <oksana.khylyk@gmail.com>
2022-11-18 22:15:00 +0100

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
 
 const galleryContainer = document.querySelector('.gallery');
 const itemMarkup = createGalleryCart(galleryItems);
